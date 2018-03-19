0e68673d512fbb517a5ccb2e6980933b90994f35
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-03-19 15:16:58 -0300

    Refactor after removing service in platform

--- src/main/java/br/com/caelum/mog/services/CoursesService.java
+++ src/main/java/br/com/caelum/mog/services/CoursesService.java
@@ -1,14 +1,30 @@
 package br.com.caelum.mog.services;
 
+import org.springframework.stereotype.Service;
+
 import br.com.caelum.mog.domains.models.Course;
 import br.com.caelum.mog.domains.models.Platform;
-import org.springframework.stereotype.Service;
+import br.com.caelum.mog.rest.ClassroomCoursesRestClient;
+import br.com.caelum.mog.rest.CoursesRestClient;
+import br.com.caelum.mog.rest.OnlineCoursesRestClient;
 
 @Service
 public class CoursesService {
 
+	private OnlineCoursesRestClient onlineClient;
+	private ClassroomCoursesRestClient classroomClient;
+
+	public CoursesService(OnlineCoursesRestClient onlineClient, ClassroomCoursesRestClient classroomClient) {
+		this.onlineClient = onlineClient;
+		this.classroomClient = classroomClient;
+	}
+	
     public Course getCourseByCodeAndPlatform(String code, Platform platform) {
-        return platform.getCourseByCode(code);
+    		return getClientByPlatform(platform).getCourseByCode(code);
     }
 
+    
+    private CoursesRestClient getClientByPlatform(Platform platform) {
+    		return platform.equals(Platform.ONLINE)? onlineClient: classroomClient;
+    }
 }
