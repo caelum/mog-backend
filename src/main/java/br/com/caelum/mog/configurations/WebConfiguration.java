ecf308fdd4f4150782781a517ae08e2a0726aaab
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-03-19 15:15:29 -0300

    Produces RestTemplate

--- src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
+++ src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.caelum.mog.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,4 +18,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*");
     }
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 }
