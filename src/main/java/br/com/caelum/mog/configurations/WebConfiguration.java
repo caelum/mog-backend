e9d7f4beabd1a54d8fed892e3cda04fdc34722a1
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-03-25 15:31:27 -0300

    Chaged deprecated class WebMvcConfigureAdapter to interface WebMvcConfigure

--- src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
+++ src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurerAdapter{
+public class WebConfiguration implements WebMvcConfigurer{
 
 
     @Value("${mog.cors.origins}")
