89f77cd486543731b032de357c64e09d7e1cf173
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-03-25 13:59:12 -0300

    Fix Cross Origin

--- src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
+++ src/main/java/br/com/caelum/mog/configurations/WebConfiguration.java
@@ -11,12 +11,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfiguration extends WebMvcConfigurerAdapter{
 
 
-    @Value("mog.cors.origins")
+    @Value("${mog.cors.origins}")
     private String origins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**").allowedOrigins(origins);
     }
 
 
