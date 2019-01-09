6c064904814414ce56f842de468b05516e6e763d
Gabriel <gabriheron@hotmail.com>
2019-01-09 18:06:05 -0200

    [Gerador de Propostas] Adding parameter to our OfferDTO

--- src/main/java/br/com/caelum/mog/domains/dtos/OfferDTO.java
+++ src/main/java/br/com/caelum/mog/domains/dtos/OfferDTO.java
@@ -3,17 +3,21 @@ package br.com.caelum.mog.domains.dtos;
 import br.com.caelum.mog.domains.models.Course;
 import br.com.caelum.mog.domains.models.Customer;
 import br.com.caelum.mog.domains.models.Offer;
+import br.com.caelum.mog.domains.models.Responsible;
 import br.com.caelum.mog.services.CoursesService;
-import org.springframework.util.Assert;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.springframework.util.Assert.hasText;
+import static org.springframework.util.Assert.notEmpty;
+
 public class OfferDTO {
 
     private String commercialName;
+    private String responsibleName;
     private List<CourseDTO> courses = new ArrayList<>();
 
     /**
@@ -22,12 +26,13 @@ public class OfferDTO {
     @Deprecated(since = "1.0.0")
     private OfferDTO() { }
 
-    public OfferDTO(String commercialName, CourseDTO... courses) {
-        Assert.hasText(commercialName, "Commercial name required");
-        Assert.notEmpty(courses, "At least one course must be informed");
+    public OfferDTO(String commercialName, String responsibleName, CourseDTO... courses) {
+        hasText(commercialName, "Commercial name required");
+        notEmpty(courses, "At least one course must be informed");
 
         this.commercialName = commercialName;
         this.courses = List.of(courses);
+        this.responsibleName = responsibleName;
     }
 
     public String getCommercialName() {
@@ -38,12 +43,18 @@ public class OfferDTO {
         return courses;
     }
 
+    public String getResponsibleName() {
+        return responsibleName;
+    }
+
     public Offer toOffer(CoursesService service) {
 
         List<Course> mappedCourses = this.courses.stream().map(dto -> dto.toCourse(service)).collect(Collectors.toList());
 
         Customer customer = new Customer(commercialName);
 
-        return new Offer(customer, mappedCourses, LocalDate.now());
+        Responsible responsible = new Responsible(responsibleName);
+
+        return new Offer(customer, mappedCourses, LocalDate.now(), responsible);
     }
 }
