e31bb229d1ad1bfcaaa6370ef8d8b36c3b0dca90
anderson-figueiredo <figueiredo.anderson@rocketmail.com>
2019-08-27 20:13:48 -0300

    [New Fields - prerequisite] Fixing test and imports

--- src/test/java/br/com/caelum/mog/services/DownloadsServiceTest.java
+++ src/test/java/br/com/caelum/mog/services/DownloadsServiceTest.java
@@ -1,14 +1,17 @@
 package br.com.caelum.mog.services;
 
-import br.com.caelum.mog.adapters.Downloadable;
-import br.com.caelum.mog.domains.models.Course;
-import br.com.caelum.mog.domains.models.Customer;
-import br.com.caelum.mog.domains.models.Offer;
-import br.com.caelum.mog.domains.models.CaelumInfo;
-import br.com.caelum.mog.domains.models.CareOf;
-import br.com.caelum.mog.domains.models.Responsible;
-import br.com.caelum.mog.domains.models.CourseSummaryItem;
-import br.com.caelum.mog.enums.CaelumDistrict;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Arrays.*;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,13 +20,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.caelum.mog.adapters.Downloadable;
+import br.com.caelum.mog.domains.models.CaelumInfo;
+import br.com.caelum.mog.domains.models.CareOf;
+import br.com.caelum.mog.domains.models.Course;
+import br.com.caelum.mog.domains.models.CourseSummaryItem;
+import br.com.caelum.mog.domains.models.Customer;
+import br.com.caelum.mog.domains.models.Offer;
+import br.com.caelum.mog.domains.models.Responsible;
+import br.com.caelum.mog.enums.CaelumDistrict;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -41,8 +46,8 @@ class DownloadsServiceTest {
         CourseSummaryItem defaultSummary = new CourseSummaryItem("Introduction", "Chapter 1", "Chapter 2");
 
         List<Course> courses = List.of(
-                new Course("Course One", "one", 10, "FJ-11", defaultSummary),
-                new Course("Course Two", "two", 20, "FJ-22", defaultSummary)
+                new Course("Course One", "one", 10, Arrays.asList("FJ-11"), defaultSummary),
+                new Course("Course Two", "two", 20, Arrays.asList("FJ-22"), defaultSummary)
         );
 
         Responsible responsible = new Responsible("Bianca Cavalcante");
